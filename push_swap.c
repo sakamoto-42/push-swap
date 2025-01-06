be12f295f175c27203794dcc23718a8996770eb9
julien <julien66140@hotmail.fr>
2025-01-06 14:41:44 +0100

    check is unique and add to list

--- push_swap.c
+++ push_swap.c
@@ -6,59 +6,54 @@
 /*   By: juduchar <juduchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 20:35:13 by juduchar          #+#    #+#             */
-/*   Updated: 2025/01/06 11:52:44 by juduchar         ###   ########.fr       */
+/*   Updated: 2025/01/06 14:36:12 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-//#include <limits.h>
+#include <limits.h>
 
-/*
+typedef struct Node {
+	int		value;
+	struct	Node *next;
+} Node;
+
+Node	*create_node(int value)
+{
+	Node *new_node = malloc(sizeof(Node));
+	if (!new_node)
+		return (NULL);
+	new_node->value = value;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+void	add_node(Node **head, int value)
+{
+	Node	*new_node;
+	Node	*current;
+
+	new_node = create_node(value);
+	if (!*head)
+		*head = new_node;
+	else
+	{
+		current = *head;
+		while (current->next)
+			current = current->next;
+		current->next = new_node;
+	}
+}
+
 // libft
-int	ft_isspace(int c)
+int	ft_is_space(int c)
 {
 	return (c == 32 || (c >= 9 && c <= 13));
 }
 
 // libft
-int	ft_atoi(const char *nptr)
-{
-	int	result;
-	int	sign;
-
-	result = 0;
-	sign = 1;
-	while (ft_isspace((int)*nptr))
-		nptr++;
-	if (*nptr == '+' || *nptr == '-')
-	{
-		if (*nptr == '-')
-			sign = -1;
-		nptr++;
-	}
-	while (ft_isdigit((int)*nptr))
-	{
-		result = (result * 10) + ((int)(*nptr) - 48);
-		nptr++;
-	}
-	return (sign * result);
-}
-
-int	ft_check_is_int(int ac, char **av)
-{
-	ft_atoi()
-}
-
-int	ft_check_error(int ac, char **av)
-{
-	if (ft_check_is_nbr(ac, av) && ft_check_is_int(ac, av) && ft_check_is_unique(ac, av))
-		return (0);
-	return (1);
-	
-}
-*/
 
 int	ft_is_digit(int c)
 {
@@ -85,6 +80,98 @@ int	ft_is_nbr(const char *str)
 		i++;
 	}
 	return (1);
+}
+
+long long	ft_strtoll(const char *str)
+{
+	long long	result;
+	int			sign;
+	int			digit;
+
+	result = 0;
+	sign = 1;
+
+	while (ft_is_space(*str))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		digit = *str - '0';
+		if (result > (LLONG_MAX - digit) / 10)
+		{
+			if (sign == 1)
+				return (LLONG_MAX);
+			else
+				return (LLONG_MIN);
+		}
+		result = result * 10 + digit;
+		str++;
+	}
+	return (result * sign);
+}
+
+int	ft_is_int(const char *str)
+{
+	long long	n;
+
+	n = ft_strtoll(str);
+	return (n >= INT_MIN && n <= INT_MAX);
+}
+
+int	ft_is_unique(Node *head, int value)
+{
+	Node	*current;
+
+	current = head;
+	while (current)
+	{
+		if (current->value == value)
+			return (0);
+		current = current->next;
+	}
+	return (1);
+}
+
+void	ft_free_list(Node *head)
+{
+	Node	*current;
+	Node	*temp;
+
+	current = head;
+	while (current)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int	result;
+	int	sign;
+
+	result = 0;
+	sign = 1;
+	while (ft_is_space((int)*nptr))
+		nptr++;
+	if (*nptr == '+' || *nptr == '-')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (ft_is_digit((int)*nptr))
+	{
+		result = (result * 10) + ((int)(*nptr) - 48);
+		nptr++;
+	}
+	return (sign * result);
 }
 
 size_t	ft_strlen(const char *s)
@@ -269,6 +356,10 @@ int	main(int argc, char **argv)
 	int		i;
 	int		j;
 	int		k;
+	int		l;
+	Node	*head;
+	int		value;
+	Node	*current;
 
 	if (argc - 1 == 0)
 		return (1);
@@ -280,7 +371,7 @@ int	main(int argc, char **argv)
 		j = 0;
 		while (current_strs[j])
 		{
-			if (!ft_is_nbr(current_strs[j]))
+			if (!ft_is_nbr(current_strs[j]) || !ft_is_int(current_strs[j]))
 			{
 				write(2, "Error\n", 6);
 				ft_free_splitted(current_strs);
@@ -301,12 +392,36 @@ int	main(int argc, char **argv)
 		}
 		i++;
 	}
+
 	k = 0;
 	while (strs[k])
 	{
 		printf("%s\n", strs[k]);
 		k++;
 	}
+
+	head = NULL;
+	l = 0;
+	while (strs[l])
+	{
+		value = ft_atoi(strs[l]);
+		if (!ft_is_unique(head, value))
+		{
+			printf("Doublon detecte : %d\n", value);
+			ft_free_list(head);
+			return (1);
+		}
+		add_node(&head, value);
+		l++;
+	}
 	ft_free_splitted(strs);
+	printf("------------------------------------\n");
+	current = head;
+	while (current)
+	{
+		printf("%d\n", current->value);
+		current = current->next;
+	}
 	return (1);
+	
 }
